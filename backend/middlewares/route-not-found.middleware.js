458b763c91f0da0874fd0575fabf506f60861afe
Utpal Pati <utpalpati8@gmail.com>
2021-07-09 11:45:24 +0000

    refactor: backend repository change

--- backend/middlewares/route-not-found.middleware.js
+++
@@ -1,5 +0,0 @@
-function routeNotFound(req, res, next) {
-  res.status(404).json({ success: false, message: "the route you're looking for couldn't be found" })
-}
-
-module.exports = { routeNotFound }
